698b7424983810822ad5fe05c3dff5b639a74237
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-11-26 21:58:36 +0900

    Feat: 토큰 재발급

--- src/hoc/auth.jsx
+++ src/hoc/auth.jsx
@@ -1,23 +1,34 @@
-/*import { useCookies } from "react-cookie";
+import moment from "moment";
+import { useEffect } from "react";
+import { useCookies } from "react-cookie";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { getUserSelector, loginState } from "../Atom/status";*/
+import { getUserSelector, loginState } from "../Atom/status";
 
 function Auth(SpecificComponent, option) {
   function AuthenticationCheck(props) {
-    /*const navigate = useNavigate();
+    const navigate = useNavigate();
     const location = useLocation();
     const userInfo = useRecoilValue(getUserSelector);
+    // const tokenInfo = useRecoilValue(getAuthenticatedSelector);
     const setIsLoggedIn = useSetRecoilState(loginState);
     const [cookies] = useCookies(["accessToken"]);
 
     
     useEffect(() => {
+      console.log(userInfo);
+      // console.log(tokenInfo);
+      console.log(window.localStorage.getItem("isAuthenticated"));
+      console.log(cookies?.accessToken);
       const isAuth = window.localStorage.getItem("isAuthenticated");
       if (userInfo === "404" && location.pathname !== "/") {
         navigate("/");
       } else {
-        if (!isAuth && cookies?.accessToken) {
+        // if(userInfo !== "404"){
+        //   const tokenInfo = useRecoilValue(getAuthenticatedSelector);
+        //   console.log(tokenInfo);
+        // }
+        if (!window.localStorage.getItem("isAuthenticated") && cookies?.accessToken) {
           // 첫 로그인 시
           window.localStorage.setItem("isAuthenticated", true);
           setIsLoggedIn({
@@ -35,7 +46,7 @@ function Auth(SpecificComponent, option) {
       }
 
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);*/
+    }, []);
     return <SpecificComponent />;
   }
   return AuthenticationCheck;
