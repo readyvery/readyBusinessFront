b35fdf6e93a8daaa5168bfc301e052c38611f6ac
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-11-30 21:53:19 +0900

    Docs: 토큰 체크 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import { message } from 'antd';
 import axios from "axios";
 import React, { Suspense } from "react";
 import { useCookies } from "react-cookie";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useSetRecoilState } from "recoil";
 import InventoryPage from "../src/pages/Inventory/Inventory";
 import Mypage from "../src/pages/Mypage/Mypage";
 import SalesPage from "../src/pages/Sales/Sales";
 import "./App.css";
+import { isAuthenticatedState, loginState } from "./Atom/status";
 import Auth from "./hoc/auth.jsx";
 import useInterval from "./hooks/useInterval.jsx";
 import HomePage from "./pages/Home/Home";
 import MainPage from "./pages/Main/MainPage.jsx";
 
 function App() {
+  const setIsLoggedIn = useSetRecoilState(loginState);
+  const setIsAuthenticated = useSetRecoilState(isAuthenticatedState);
   const [cookies, , removeCookies] = useCookies();
   const navigate = useNavigate();
   const apiUrl = process.env.REACT_APP_API_ROOT;
@@ -24,13 +28,15 @@ function App() {
   const NewMyPage = Auth(Mypage, true);
 
   const expiredTime = 1000 * 60 * 60 * 24;
+  // const expiredTime = 60000
   // const expiredTime = 65000;
   useInterval(() => {
-    // console.log(cookies.refreshToken);
+    console.log(cookies);
     // if (
     //   cookies.refreshToken !== "undefined" &&
     //   cookies.refreshToken !== undefined &&
     //   cookies.refreshToken
+
     // ) {
     if(cookies.accessToken){
       const config = {
@@ -47,12 +53,33 @@ function App() {
         })
         .catch((err) => {
           console.log(err);
-          alert("토큰이 만료되었습니다. 로그인을 진행해주세요.");
+          // const config = {
+          //   withCredentials: true,
+          // };
+      
+          // axios
+          //   .get(apiUrl + "/api/v1/user/logout", config)
+          //   .then((response) => {
+          //     console.log(response);
+          //     setIsAuthenticated(false);
+          //     setIsLoggedIn({
+          //       accessToken: null,
+          //       expiredTime: null,
+          //     });
+          //     removeCookies("accessToken");
+          //     removeCookies("JSESSIONID");
+          //     window.localStorage.setItem("isAuthenticated", false);
+          //   })
+          //   .catch((error) => {
+          //     message.info("관리자에게 문의하세요.");
+          //     navigate("/");
+          //   });
+          message.info("토큰이 만료되었습니다. 로그인을 진행해주세요.");
           navigate("/");
         });
       }
     // }
-  }, expiredTime - 60000);
+  }, expiredTime);
   return (
     <div className="App">
       <RecoilRoot>
