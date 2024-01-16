46d3428b0865973615488857740550c1a48e489d
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-01-16 15:36:29 +0900

    Design: 회원가입 페이지 정보 입력창 UI

---
+++ src/components/signup/UserInput/UserInputId/UserInputId.jsx
@@ -0,0 +1,63 @@
+import LoginChkAlrm from "../../../login/LoginChkAlrm/LoginChkAlrm";
+import "./UserInputId.css";
+import { useState } from "react";
+
+const UserInputId = () => {
+    // 아이디 입력 값 처리
+    const [inputId, setInputId] = useState("");
+    const [inputIdError, setInputIdError] = useState(false);
+    const isInputIdLength = (inputId) => {
+        const minLength = 8;
+        return inputId.length >= minLength;
+      };
+      const isInputIdCombinationValid = (inputId) => {
+        const hasLetter = /[a-zA-Z]/.test(inputId);
+        const hasNumber = /\d/.test(inputId);
+        return hasLetter && hasNumber;
+      };
+      const handleInputChange = (e) => {
+        const userInputId = e.target.value;
+        setInputId(userInputId);
+        validateUserInputId(userInputId);
+      };
+      const validateUserInputId = (userInputId) => {
+        if (!isInputIdLength(userInputId)) {
+            setInputIdError(true);
+        } else if (!isInputIdCombinationValid(userInputId)) {
+            setInputIdError(true);
+        } else {
+            setInputIdError(false);
+        }
+      };
+
+    return(
+        <div className="signup-page-content-id-wrapper">
+            <label className="signup-page-content-id-label-style">아이디</label>
+            <div className="signup-page-content-id-label-plus-text">
+                <div>영문+숫자+특수기호 8자 이상</div>
+                <button className="duplication-check-button">중복 확인</button>
+            </div>
+            <div>
+                <input 
+                    id="userid" 
+                    type="text" 
+                    placeholder="아이디" 
+                    required name="userid"             
+                    value={inputId}
+                    onChange={handleInputChange}
+                    className="signup-page-content-id-input"/>
+            </div>
+            {inputIdError ? (
+                    <LoginChkAlrm icon={"X"} paddingSize={"0.45rem"}>
+                    { !isInputIdLength(inputId)
+                    ? "8자 이상 입력해야 합니다."
+                    : !isInputIdCombinationValid(inputId)
+                    ? "영문+숫자의 조합이어야 합니다."
+                    : null}
+                </LoginChkAlrm>
+                ) : <div style={{minHeight: "0.9rem"}}/>
+            }
+        </div>
+    )
+}
+export default UserInputId;
