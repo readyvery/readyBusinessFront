2707c321575fef86ad867cc6a09a3b2dc4a6c306
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-01-17 21:24:31 +0900

    Design: 회원가입 페이지 가게 정보 입력란  디자인 수정 및 모바일 호환 추가

--- src/pages/Signup/Verification/VerificationPage.jsx
+++ src/pages/Signup/Verification/VerificationPage.jsx
@@ -5,13 +5,17 @@ import Container from "../../../components/login/Container/Container";
 import UserInputStoreInformation from "../../../components/signup/UserInput/UserInputStoreInformation/UserInputStoreInformation";
 
 function VerificationPage () {
-
+    const is480 = window.innerWidth <= 480;
+    const containerSize = is480
+      ? ["20rem", "43.85rem", "1.5rem"]
+      : ["35rem", "72.55rem", "3.8rem"];
+  
     return(
-        <Container title="회원가입" containerWidth="35rem" containerHeight="72.55rem" logoMarginTop="3.8rem" logoMarginBottom="2.7rem" >
+        <Container title="회원가입" containerWidth={containerSize[0]} containerHeight={containerSize[1]} logoMarginTop={containerSize[2]} logoMarginBottom="2.7rem">
                 <CircleNumBar step="2"/>
                 <div className="signup-page-content-container">
                     <UserInputStoreInformation/>
-                    <div className="signup-page-next-button">
+                    <div className="signup-page-verification-next-button">
                         <RedButton>승인요청 신청</RedButton>
                     </div>
                 </div>
