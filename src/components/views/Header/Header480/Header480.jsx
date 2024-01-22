b2f662a99ef1a05dada45c376c567da263c3a884
imi21123 <imi21123@naver.com>
2024-01-22 19:58:33 +0900

    Feat: SideMenu 완성

--- src/components/views/Header/Header480/Header480.jsx
+++ src/components/views/Header/Header480/Header480.jsx
@@ -1,36 +1,53 @@
 import menu from "../../../../assets/icons/Header/ic_round-menu.svg";
 import logo from "../../../../assets/icons/Header/LOGO.svg";
+import SideMenu from "../../SideMenu/SideMenu";
 import "./Header480.css";
 import { useState } from "react";
 
 export default function Header480() {
   const [isOperation, setIsOperation] = useState(false);
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
   const handleOperation = () => {
     setIsOperation((prev) => !prev);
   };
+
+  const handleSidebar = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+  };
+
   const isOperationBackground = isOperation
     ? "rgba(131, 131, 131, 1)"
     : "rgba(216, 35, 86, 1)";
 
   return (
-    <header className="header480">
-      <img src={menu} alt="menu" className="header480__menu" />
-      <img src={logo} alt="logo" className="header__logo" />
+    <>
+      <SideMenu isOpen={isSideMenuOpen} toggleSidebar={handleSidebar} />
 
-      <span className="business-status">
-        {isOperation ? "영업종료" : "영업중"}
-      </span>
-      <div
-        className="loginControl-button-wrapper-css"
-        onClick={() => handleOperation()}
-        style={{ backgroundColor: isOperationBackground }}
-      >
-        {isOperation ? (
-          <div className="loginControl-button-isopen-css"></div>
-        ) : (
-          <div className="loginControl-button-isclose-css"></div>
-        )}
-      </div>
-    </header>
+      <header className="header480">
+        <img
+          src={menu}
+          alt="menu"
+          className="header480__menu"
+          onClick={handleSidebar}
+        />
+        <img src={logo} alt="logo" className="header__logo" />
+
+        <span className="business-status">
+          {isOperation ? "영업종료" : "영업중"}
+        </span>
+        <div
+          className="loginControl-button-wrapper-css"
+          onClick={() => handleOperation()}
+          style={{ backgroundColor: isOperationBackground }}
+        >
+          {isOperation ? (
+            <div className="loginControl-button-isopen-css"></div>
+          ) : (
+            <div className="loginControl-button-isclose-css"></div>
+          )}
+        </div>
+      </header>
+    </>
   );
 }
