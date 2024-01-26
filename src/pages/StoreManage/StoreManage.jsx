afe94f9215ff060a62ace63cb22a69e0053857cb
imi21123 <imi21123@naver.com>
2024-01-26 18:08:48 +0900

    Style: HeaderBack480 css 수정

--- src/pages/StoreManage/StoreManage.jsx
+++ src/pages/StoreManage/StoreManage.jsx
@@ -1,12 +1,16 @@
 import Footer from "../../components/views/Footer/Footer";
 import HeaderBack480 from "../../components/views/Header/Header480/HeaderBack480/HeaderBack480";
 import HeaderMain from "../../components/views/Header/HeaderMain/HeaderMain";
+import "./StoreManage.css";
 
 const StoreManage = () => {
   return (
     <div className="store">
       <HeaderMain />
       <HeaderBack480 pageName="매장관리" />
+      <div className="store__notice">
+        심사 요청 후 심사완료까지 2~3일 소모될 수 있습니다.
+      </div>
 
       <Footer />
     </div>
