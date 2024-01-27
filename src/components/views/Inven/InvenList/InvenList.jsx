ee8e069a8591717471053a64a1395f73c3cbe828
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-01-27 17:37:14 +0900

    Docs: inventory 컴포넌트 경로 변경

--- src/components/views/Inven/InvenList/InvenList.jsx
+++ src/components/views/Inven/InvenList/InvenList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import InvenBox from "./InvenBox";
+import InvenBox from "../InvenBox/InvenBox";
 import "./InvenList.css";
 
 const InvenList = ({ category, invenList, handleModal }) => (
   <div className="mainInven-category-content__wrapper">
-        {invenList?.categorys
-          ?.filter((cate) => category === "전체" || cate.name === category)
-          ?.map((categoryItem) =>
+    {invenList?.categorys
+      ?.filter((cate) => category === "전체" || cate.name === category)
+      ?.map((categoryItem) =>
         categoryItem?.foodies?.map((foodItem) => (
           <React.Fragment key={foodItem.idx}>
             <InvenBox
