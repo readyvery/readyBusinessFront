129b62a2b2ba3587b4c07f612694071350483360
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-02-05 15:48:56 +0900

    Docs: 회원가입 번호인증 rem 단위 수정

--- src/pages/Signup/PhoneAuth/PhoneAuthPage.jsx
+++ src/pages/Signup/PhoneAuth/PhoneAuthPage.jsx
@@ -3,27 +3,33 @@ import CircleNumBar from "../../../components/signup/CircleNumBar/CircleNumBar";
 import UserInputNumber from "../../../components/signup/UserInput/UserInputNumber/UserInputNumber";
 import "./PhoneAuthPage.css";
 
-function PhoneAuthPage () {
-    // const userId = useRecoilValue(userIdState);
-    // const userPassword = useRecoilValue(userPasswordState);
-    // const userName = useRecoilValue(userNameState);
-    // console.log(userId);
-    // console.log(userPassword);
-    // console.log(userName);
-    
-    const is480 = window.innerWidth <= 480;
-    const containerSize = is480
-      ? ["20rem", "30rem", "2.05rem"]
-      : ["30rem", "40rem", "4.5rem"];
-    
-    return(
-        <Container title="시작하기" containerWidth={containerSize[0]} containerHeight={containerSize[1]} logoMarginTop={containerSize[2]} logoMarginBottom="2.45rem">
-                <CircleNumBar step="1"/>
-                <div className="signup-page-content-phone-container">
-                    <UserInputNumber/>
-                </div>
-        </Container>
-    )
+function PhoneAuthPage() {
+  // const userId = useRecoilValue(userIdState);
+  // const userPassword = useRecoilValue(userPasswordState);
+  // const userName = useRecoilValue(userNameState);
+  // console.log(userId);
+  // console.log(userPassword);
+  // console.log(userName);
+
+  const is480 = window.innerWidth <= 480;
+  const containerSize = is480
+    ? ["25rem", "37.5rem", "2.56rem", "2.31rem"]
+    : ["37.5rem", "50rem", "5.63rem", "3.88rem"];
+
+  return (
+    <Container
+      title="시작하기"
+      containerWidth={containerSize[0]}
+      containerHeight={containerSize[1]}
+      logoMarginTop={containerSize[2]}
+      logoMarginBottom={containerSize[3]}
+    >
+      <CircleNumBar step="1" />
+      <div className="signup-page-content-phone-container">
+        <UserInputNumber />
+      </div>
+    </Container>
+  );
 }
 
 export default PhoneAuthPage;
