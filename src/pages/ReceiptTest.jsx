6370c23a3a7a54572ec48dfbd61772be6f9e51f7
choiyoubin <48716638+choiyoubin@users.noreply.github.com>
2024-02-15 10:28:15 +0900

    Feat/Receipt-Test

--- src/pages/ReceiptTest.jsx
+++ src/pages/ReceiptTest.jsx
@@ -1,34 +1,20 @@
-import { Printer, Text, render } from 'react-thermal-printer';
+import { render } from 'react-thermal-printer';
+import UserReceipt from '../components/UserReceipt';
 
 const ReceiptTest = (props) => {
   	// const orderInfo = props.orderInfo;
   
     const onClickPrintHandler = async () => {
-      // const data = await render(UserReceipt());
-      // const port = await window.navigator.serial.requestPort();
-      // await port.open({ baudRate: 9600 });
-      // const writer = port.writable?.getWriter();
-      // if (writer !== null) {
-      //   await writer.write(data);
-      //   await writer.releaseLock();
-      // }
-      // await port.close({ baudRate: 9600 });
-
-      const data = await render(
-        <Printer type="epson">
-          <Text>Hello World</Text>
-        </Printer>
-      );
-      
+      const data = await render(UserReceipt());
       const port = await window.navigator.serial.requestPort();
       await port.open({ baudRate: 9600 });
-      
       const writer = port.writable?.getWriter();
-      if (writer != null) {
+      if (writer !== null) {
         await writer.write(data);
-        writer.releaseLock();
+        await writer.releaseLock();
       }
       await port.close({ baudRate: 9600 });
+
   };
   
   
