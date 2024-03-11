115f20aca6a1fbaecd0af88de7cbaf019ed60451
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-03-11 20:05:37 +0900

    Feat: 주문 접수 & 거절 API 연동

--- src/components/views/Home/ReciptBox.jsx
+++ src/components/views/Home/ReciptBox.jsx
@@ -1,7 +1,10 @@
 import React, { useContext, useMemo } from "react";
 import cherry from "../../../assets/icons/cherry.svg";
+import useAcceptOrder from "../../../hooks/useAcceptOrder";
+import useCancelOrder from "../../../hooks/useCancelOrder";
 import { HomeContext } from "../../../pages/OrderManage/Home";
 import "../../../pages/OrderManage/Receipt.css";
+import ReceiptTest from "../../../pages/ReceiptTest";
 import ReciptModal from "./ReciptModal";
 
 export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
@@ -14,99 +17,60 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
     ]), []);
 
     const receiveList = useMemo(() => ([
-      "즉시", 
-      "5분", 
-      "10분", 
-      "15분", 
-      "20분", 
-      "25분", 
-      "30분"
+      0, 
+      5, 
+      10, 
+      15, 
+      20, 
+      25, 
+      30
     ]), []);
+
+    const cancelOrder = useCancelOrder();
+    const acceptOrder = useAcceptOrder();
   
-    const cancelOrder = (e) => {
+    const handleCancel = async(e) => {
       setModalIdx(0);
-      // const config = {
-      //   withCredentials: true,
-      // };
-  
-      // const body = {
-      //   orderId: orderProps.orderId,
-      //   status: "CANCEL",
-      //   rejectReason: e.target.innerText,
-      // };
-      // console.log(body);
-  
-      // axios
-      //   .post(`${apiUrl}/api/v1/order/cancel`, body, config)
-      //   .then((res) => {
-      //     console.log(res);
-      //     if (res.status === 200) {
-      //       message.info("취소되었습니다.");
-      //       setRefuseModal((prev) => !prev);
-      //       // 데이터 다시 fetch
-      //       fetchData();
-      //       // select된 데이터 변경
-      //       setStatus("null");
-      //       setOrder(null);
-      //     }
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //     setRefuseModal((prev) => !prev);
-      //     // 데이터 다시 fetch
-      //     fetchData();
-      //     // select된 데이터 변경
-      //     setStatus("null");
-      //     setOrder(null);
-      //   });
+      console.log(e);
+      try{
+        await cancelOrder(
+          context?.selectedMenu[0]?.orderId, 
+          e
+        )
+      } catch (error){
+        console.error(error);
+      }
     };
   
     const handleMake = async (e) => {
       setModalIdx(0);
-      // const config = {
-      //   withCredentials: true,
-      // };
-  
-      // const body = {
-      //   orderId: orderProps.orderId,
-      //   status: "MAKE",
-      //   time: e.target.innerText === "즉시완료" ? 0 : parseInt(e.target.innerText.split("분")[0]),
-      // };
-      // console.log(body.time);
-  
-      // try {
-      //   const res = await axios.post(
-      //     `${apiUrl}/api/v1/order/complete`,
-      //     body,
-      //     config
-      //   );
-      //   console.log(res);
-      //   if (res.status === 200) {
-      //     message.success("접수되었습니다.");
-      //     setReceiveModal((prev) => !prev);
-      //     // 데이터 다시 fetch
-      //     await fetchData();
-      //     // select된 데이터 변경
-      //     // 클릭 시 스타일 변화
-      //     setStatus("null");
-      //     setOrder(null);
-      //   }
-      // } catch (err) {
-      //   console.log(err);
-      // }
+      console.log(context?.selectedMenu);
+      try{
+        await acceptOrder(
+          context?.selectedMenu[0]?.orderId, 
+          e
+        )
+      } catch (error){
+        console.error(error);
+      }
     };
 
     const selectedInfo = context.selectedMenu;
     console.log(selectedInfo);
 
-
     return(
       <div className="receiptWrapper">
+        {/* <img
+            src={close}
+            className="BackIcon"
+            alt="BackIcon"
+            onClick={() => setModalIdx(0)}
+          /> */}
         {
           selectedInfo?.length ? (
             <>
               <div className="receiptHeader">
-                <span className="receipt-header"> 주문번호 {selectedInfo[0]?.idx}</span>
+                <span className="receipt-header"> 주문번호 {selectedInfo[0]?.orderNum}</span>
                 {children}
             </div>
             <div className="receiptTextBox">
@@ -120,15 +84,13 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
             <div className="receiptTextBox">
                 <span className="receipt-text">고객연락처</span>
                 <span className="receipt-text">
-                    {/* {orderProps?.phone?.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3")} */}
                     {selectedInfo[0]?.phone}
                 </span>
             </div>
             <div className="receiptTextBox">
                 <span className="receipt-text">수령방식</span>
                 <span className="receipt-text">
-                    {/* {orderProps?.phone?.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3")} */}
-                    {selectedInfo[0]?.pickUp}
+                    {selectedInfo[0]?.pickUp === 1 ? "픽업" : "매장"}
                 </span>
             </div>
             <div className="receipt-divider" />
@@ -167,8 +129,7 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
             <div className="receiptTextBox">
               <span className="receipt-text">할인금액</span>
               <span className="receipt-text">
-                {/* {orderProps?.couponUsed ? "(-) 500원" : "0원"} */}
-                (-)500원
+                {selectedInfo[0]?.couponUsed ? "(-) 500원" : "0원"}
               </span>
             </div>
             <div className="receiptTextBox">
@@ -178,10 +139,17 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
               </span>
             </div>
 
+            {
+              context?.selectedMenu &&
+              context?.selectedMenu.length > 0 &&
+              context?.selectedMenu[0]?.progress &&
+              context?.selectedMenu[0]?.progress === "MAKE" && 
+              <ReceiptTest color="white"/>
+            }
+
             {/* 주문거부모달창 */}
             {modalIdx === 1 && (
               <ReciptModal 
-                handleOrder={cancelOrder} 
                 closeModal={() => setModalIdx(0)}
                 title="접수 거부 사유를 선택해주세요"
               >
@@ -190,7 +158,7 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
                     <React.Fragment key={idx}>
                       <div
                         className="modal-box-choose-btn"
-                        onClick={cancelOrder}
+                        onClick={() => handleCancel(text)}
                       >
                         {text}
                       </div>
@@ -203,7 +171,6 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
             {/* 주문수락모달창 */}
             {modalIdx === 2 && (
               <ReciptModal 
-                handleOrder={handleMake} 
                 closeModal={() => setModalIdx(0)}
                 title="제조 시간을 선택해주세요"
               >
@@ -216,9 +183,9 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
                         <div className="modal-box-choose-btn__row">
                           <div
                             className="modal-box-chooseTime-btn column"
-                            onClick={handleMake}
+                            onClick={() => handleMake(text)}
                           >
-                            {text}
+                            즉시
                           </div>
                         </div>
                       </React.Fragment>
@@ -227,9 +194,9 @@ export default function ReceiptBox ({children, modalIdx, setModalIdx}) {
                         <React.Fragment key={idx}>
                           <div
                             className="modal-box-chooseTime-btn-between row"
-                            onClick={handleMake}
+                            onClick={() => handleMake(text)}
                           >
-                            {text}
+                            {text}분
                           </div>
                         </React.Fragment>
                       )
