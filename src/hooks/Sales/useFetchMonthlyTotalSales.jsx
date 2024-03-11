e07847d986e335ff2e49b27ff0782096b7945d97
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-03-11 19:13:44 +0900

    Feat: 주문 건수, 총매출 조회 연결

---
+++ src/hooks/Sales/useFetchMonthlyTotalSales.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { loginState } from "../../Atom/status";
+
+const apiRoot = process.env.REACT_APP_API_ROOT;
+const apiVer = "api/v1";
+const apiUrl = `${apiRoot}/${apiVer}/sale/management/amount/monthly`;
+
+const useFetchMonthlyTotalSales = (monday) => {
+  const [monthlyTotalSales, setMonthlyTotalSales] = useState(0);
+  const token = useRecoilValue(loginState);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token.accessToken}`,
+    },
+  };
+  const body = {
+    monday: monday,
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      axios
+        .post(`${apiUrl}`, body, config)
+        .then((res) => {
+          if (res.data.success) {
+            if (res.data.totalMoney !== null)
+              setMonthlyTotalSales(res.data.totalMoney);
+            else {
+              setMonthlyTotalSales(0);
+            }
+          }
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [monday]);
+  return monthlyTotalSales;
+};
+
+export default useFetchMonthlyTotalSales;
