b45e10f2ea86d208585d8033995362f04e3d4f1b
KKangHHee <137751841+KKangHHee@users.noreply.github.com>
2024-03-12 21:34:40 +0900

    Fix: 비밀번호 찾기 UI 수정

--- src/pages/Find/FindPasswordPage/FindPasswordPage.jsx
+++ src/pages/Find/FindPasswordPage/FindPasswordPage.jsx
@@ -1,13 +1,14 @@
+import { message } from "antd";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
-import { findState } from "../../../Atom/status";
-import CertificationInput from "../../../components/login/Certification/CertificationInput/CertificationInput";
+import { findPasswordState } from "../../../Atom/status";
 import Container from "../../../components/login/Container/Container";
-
+import RedButton from "../../../components/login/redButton/RedButton";
+import "./FindPasswordPage.css";
 function FindPasswordPage() {
-  const setIsFind = useSetRecoilState(findState);
-  setIsFind(2);
-  
+  // 화면 사이즈 검사
   const [is480, setIs480] = useState(window.innerWidth <= 480);
   const containerSize = is480
     ? ["25rem", "37.5rem", "4.13rem", "6.5rem"]
@@ -23,6 +24,55 @@ function FindPasswordPage() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  // 1. 아이디 조회 -> 조회성공 시 전화번호 입력페이지
+  const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const setPasswordState = useSetRecoilState(findPasswordState);
+  const handleCheckId = (event) => {
+    setId(event.target.value);
+  };
+  const apiRoot = process.env.REACT_APP_API_ROOT;
+  const apiVer = "api/v1";
+  // 해당 바꾸기
+  const apiUrl = `${apiRoot}/${apiVer}/ceo/find/password/email`;
+  const handleIdDuplicateCheck = async () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (id === "") {
+      message.error("아이디를 입력해주세요.");
+      return;
+    } else if (emailRegex) {
+      try {
+        const response = await axios.post(`${apiUrl}`, {
+          email: id,
+        });
+
+        if (response.data.success) {
+          setPasswordState({
+            email: id,
+            phoneNumber: response.data.message,
+          });
+          message.success("아이디 조회에 성공했습니다.");
+          navigate(`/find/password/change`);
+        }
+      } catch (error) {
+        // 404에러 처리(사용자 id가 db에 없을 경우)
+        if (axios.isAxiosError(error)) {
+          if (error.response && error.response.status === 404) {
+            message.error("존재하지 않는 아이디 입니다.");
+            setId("");
+          }
+        }
+        // 다른 오류 처리
+        else {
+          message.error("아이디 조회 요청에 실패했습니다.");
+        }
+      }
+    } else {
+      message.error("올바른 메일 주소를 입력해주세요.");
+      return;
+    }
+  };
   return (
     <Container
       title={"비밀번호 찾기"}
@@ -32,18 +82,19 @@ function FindPasswordPage() {
       logoMarginBottom={containerSize[3]}
     >
       <div>
-        <CertificationInput
-          id="userid"
-          type="text"
-          placeholder="아이디"
-          requiredname="userid"
-          text="조회"
-          buttonText="다음"
-        />
+        <div className="loginpage-find-password-check-id-wrapper">
+          <input
+            type="text"
+            placeholder="아이디"
+            value={id}
+            onChange={handleCheckId}
+          />
+        </div>
+        <div className="loginpage-find-password-next-button">
+          <RedButton onClick={handleIdDuplicateCheck}>다음</RedButton>
+        </div>
       </div>
     </Container>
-
-      
   );
 }
 export default FindPasswordPage;
