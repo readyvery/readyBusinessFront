fc3ea271aa95084b8a3a98342a258bdff44db7aa
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-03-14 14:44:04 +0900

    Docs: config 수정

---
+++ src/config/index.js
@@ -0,0 +1,26 @@
+export const config = {
+    statusList: [
+        {
+            id: 1,
+            name: "처리중"
+        },
+        {
+            id: 2,
+            name: "완료"
+        }
+    ], 
+    refuseList: [
+        "재료 소진", 
+        "가게 사정", 
+        "기타"
+    ],
+    receiveList: [
+        0, 
+        5, 
+        10, 
+        15, 
+        20, 
+        25, 
+        30
+    ]
+}
