7e40beef74cb2580a536c641640f7b652ba95a2d
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-03-14 14:37:07 +0900

    Feat: 주문 취소 api useMutation으로 변경

--- src/hooks/useCancelOrder.jsx
+++ src/hooks/useCancelOrder.jsx
@@ -1,35 +1,43 @@
 import { message } from "antd";
-import { useRecoilValue } from "recoil";
-import { loginState } from "../Atom/status";
+import { useMutation, useQueryClient } from "react-query";
 import commonApis from "../util/commonApis";
 
 const useCancelOrder = () => {
-    const token = useRecoilValue(loginState);
-    const cancelOrder = async (orderId, rejectReason) => {
-        console.log(orderId, rejectReason);
-        commonApis.post("/order/cancel", {
-            orderId: orderId,
+    const queryClient = useQueryClient();
+    const token = localStorage.getItem("accessToken");
+    
+    const { mutate: cancelOrder, isLoading, isError, error } = useMutation(
+        ( e ) => commonApis.post("/order/cancel", {
+            orderId: e.split("@")[0],
             status: "CANCEL",
             time: 0,
-            rejectReason: rejectReason,
-        },
-        {
+            rejectReason: e.split("@")[1],
+        }, {
             headers: {
-                Authorization: `Bearer ${token.accessToken}`
-            }
-        })
-            .then((res) => {
-                console.log(res);
-                if(res.status === 200 && res.data.success === true){
+                Authorization: `Bearer ${token}`,
+            },
+        }),
+        {
+            onSuccess: (data) => {
+                if (data.status === 200 && data.data.success === true) {
+                    message.destroy();
                     message.success("주문 취소되었습니다.");
+                    queryClient.invalidateQueries({queryKey: ["get-wait"]});
+                } else {
+                    message.destroy();
+                    message.error("주문 취소 실패. 다시 시도해주세요.");
+                    queryClient.invalidateQueries({queryKey: ["get-wait"]});
                 }
-            })
-            .catch((err) => {
-                console.log(err);
-                message.error("주문 취소 중 오류가 발생하였습니다.")
-            });
-    }
-    return cancelOrder;
+            },
+            onError: (err) => {
+                console.error(err);
+                message.destroy();
+                message.error("주문 취소 중 오류가 발생하였습니다.");
+            },
+        }
+    );
+
+    return { cancelOrder, isLoading, isError, error };
 }
 
 export default useCancelOrder;
