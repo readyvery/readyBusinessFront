515e81ba37c54035dc9ac08707e9e98974ddaa0d
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-03-27 02:52:37 +0900

    Feat: 모든 페이지 Auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 // import Header2 from "../src/components/views/Header/Header2";
 // import Auth from "./hoc/auth.jsx";
 import ApplicationForm from "./components/signup/ApplicationForm/ApplicationForm.jsx";
+import Auth from "./hoc/Auth.jsx";
 import FindIdPage from "./pages/Find/FindIdPage/FindIdPage.jsx"; //아이디 찾기-전화번호 인증
 import NoneFindIdPage from "./pages/Find/FindIdPage/NoneFindIdPage/NoneFindIdPage.jsx"; //아이디 찾기 결과-회원 X
 import UserFindIdPage from "./pages/Find/FindIdPage/UserFindIdPage/UserFindIdPage.jsx"; //아이디 찾기 결과-아이디 반환
@@ -22,7 +23,6 @@ import SignupPage from "./pages/Signup/SignupPage.jsx";
 import TermsPage from "./pages/Signup/Terms/TermsPage.jsx";
 import VerificationPage from "./pages/Signup/Verification/VerificationPage.jsx";
 import StoreManage from "./pages/StoreManage/StoreManage.jsx";
-import Auth from "./utils/Auth.jsx";
 
 function App() {
   //const [cookies, , removeCookies] = useCookies();
@@ -30,16 +30,16 @@ function App() {
 
   // 로그인 필요없는 페이지
   // sms인증 구현으로 인한 AUTH잠시 해제
-  // const NewSignupPage = Auth(SignupPage, false); // 회원가입
+  const NewSignupPage = Auth(SignupPage, false); // 회원가입
   const NewLoginPage = Auth(LoginPage, false); // 로그인
   // sms인증 구현으로 인한 AUTH잠시 해제
-  // const NewFindIdPage = Auth(FindIdPage, false); //아이디 찾기-전화번호 인증
+  const NewFindIdPage = Auth(FindIdPage, false); //아이디 찾기-전화번호 인증
   const NewNoneFindIdPage = Auth(NoneFindIdPage, false); //아이디 찾기 결과-회원 X
   const NewUserFindIdPage = Auth(UserFindIdPage, false); //아이디 찾기 결과-아이디 반환
   const NewFindPasswordPage = Auth(FindPasswordPage, false); //비밀번호 찾기 - 아이디 조회
   const NewChangeNewPasswordPage = Auth(ChangeNewPasswordPage, false); //비밀번호 변경 - 전화번호 인증
   const NewChangePasswordPage = Auth(ChangePasswordPage, false); //비밀번호 변경 - 전화번호 인증
-  //const NewPhoneAuthPage = Auth(PhoneAuthPage, false); // 휴대폰 인증
+  const NewPhoneAuthPage = Auth(PhoneAuthPage, false); // 휴대폰 인증
   const NewTermsPage = Auth(TermsPage, false); // 이용약관 페이지
 
   // 로그인 필수 페이지
@@ -50,7 +50,7 @@ function App() {
   const NewJudgeResultsRejectPage = Auth(JudgeResultsRejectPage, true, 3); // 입점 심사 반려 페이지
   const NewMainPage = Auth(MainPage, true, 4); // 메인페이지 (4, 5)
   const NewStoreManage = Auth(StoreManage, true, 4); // 매장관리 페이지 (4, 5)
-  // const NewOrderManagementPage = Auth(OrderManagePage, true, 5); // 주문관리
+  const NewOrderManagementPage = Auth(OrderManagePage, true, 5); // 주문관리
   const NewInventoryPage = Auth(InventoryPage, true, 5); // 재고관리
   const NewSalesPage = Auth(SalesManage, true, 5); // 매출관리
   const NewMyPage = Auth(MyPage, true, 5); // 마이페이지
@@ -60,7 +60,7 @@ function App() {
       <div>
         <div className="App">
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            <Route path="/" element={<NewLoginPage />} />
             <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
           </Routes>
         </div>
@@ -73,13 +73,12 @@ function App() {
         <Routes>
           <Route path="/main" element={<NewMainPage />} />
           <Route path="/store" element={<NewStoreManage />} />
-          <Route path="/order" element={<OrderManagePage />} />
+          <Route path="/order" element={<NewOrderManagementPage />} />
           <Route path="/inventory" element={<NewInventoryPage />} />
           <Route path="/sales" element={<NewSalesPage />} />
           <Route path="/mypage" element={<NewMyPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          {/* 추가 */}
-          <Route path="/signup/auth/phone" element={<PhoneAuthPage />} />
+          <Route path="/signup" element={<NewSignupPage />} />
+          <Route path="/signup/auth/phone" element={<NewPhoneAuthPage />} />
           <Route
             path="/signup/auth/verification"
             element={<NewVerificationPage />}
@@ -95,7 +94,7 @@ function App() {
             element={<NewJudgeResultsRejectPage />}
           />
           <Route path="/login" element={<NewLoginPage />} />
-          <Route path="/find/id" element={<FindIdPage />} />
+          <Route path="/find/id" element={<NewFindIdPage />} />
           <Route path="/find/id/search" element={<NewUserFindIdPage />} />
           <Route path="/find/id/none" element={<NewNoneFindIdPage />} />
           <Route path="/find/password" element={<NewFindPasswordPage />} />
